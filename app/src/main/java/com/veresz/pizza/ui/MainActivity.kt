75d35a350048e421df0fd06ea178eb6b3933262a
Zoltan Veres <z.veres@mito.hu>
2019-08-16 15:18:34 +0200

    Initial commit

---
+++ app/src/main/java/com/veresz/pizza/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.veresz.pizza.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.veresz.pizza.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
